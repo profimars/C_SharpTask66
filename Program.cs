9e3ad3d85a6fc6568d985a7bb121d18b2a580d44
Марс <profimars@mail.ru>
2023-04-02 21:28:07 +0300

    Добавил задачу C_SharpTask66

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//https://github.com/profimars/C_SharpTask66
+
+int Sum(int M, int N)
+{
+    if (M == N) return M;
+    return N + Sum(M, N - 1);
+}
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(M, N));
